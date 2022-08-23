0a2269fa5e982ba296cfb9d483eb6a01fb301cfa
汪永晖 <wangyonghui@guokang.com>
2022-08-23 13:23:17 +0800

    feat: init

---
+++ src/main/java/top/alexmmd/chat/CommandLineChatApplication.java
@@ -0,0 +1,16 @@
+package top.alexmmd.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wyh
+ */
+@SpringBootApplication
+public class CommandLineChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommandLineChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/alexmmd/chat/CommandLineChatApplicationTests.java
@@ -0,0 +1,13 @@
+package top.alexmmd.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandLineChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
